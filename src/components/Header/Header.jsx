606ea91c9102585c1de8855ed09f231665aa4b90
Anushka Bag <bag82262@gmail.com>
2025-02-01 20:55:38 +0530

    Header Updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,8 @@ import {
   faPlane,
   faCar,
   faTaxi,
+  faCalendarDays,
+  faPerson,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Header.css";
@@ -12,25 +14,50 @@ function Header() {
     <div className="header">
       <div className="headerContainer">
         <div className="headerList">
-          <div className="headerListItem">
+          <div className="headerListItem active">
             <FontAwesomeIcon icon={faBed} />
             <span>Stays</span>
           </div>
-          <div className="headerListItem">
+          <div className="headerListItem active">
             <FontAwesomeIcon icon={faPlane} />
             <span>Flights</span>
           </div>
-          <div className="headerListItem">
+          <div className="headerListItem active">
             <FontAwesomeIcon icon={faCar} />
             <span>Car rentals</span>
           </div>
-          <div className="headerListItem">
+          <div className="headerListItem active">
             <FontAwesomeIcon icon={faBed} />
             <span>Attractions</span>
           </div>
-          <div className="headerListItem">
+          <div className="headerListItem active">
             <FontAwesomeIcon icon={faTaxi} />
             <span>Airpot Taxis</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">
+          Seamless Reservations, Hassle-Free Experiences!
+        </h1>
+        <p className="headerDesc">
+          Book your stays, tickets, and experiences with ease. Fast, reliable,
+          and hassle-free reservations—anytime, anywhere!
+        </p>
+        <button className="headerBtn">Sign in/ Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span className="headerSearchText">date to date</span>
+          </div>
+          <div className="headerSearchItem">
+            <button className="headerBtn">Search</button>
           </div>
         </div>
       </div>
