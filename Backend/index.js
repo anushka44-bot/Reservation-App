8a1ac0e7739d5934abd99e29e9bb2a9201abdcbe
Anushka Bag <bag82262@gmail.com>
2025-06-05 11:49:20 +0530

    Basic backend setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,27 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import authRoute from "./routes/auth.js"
+import hotelsRoute from "./routes/hotels.js"
+import usersRoute from "./routes/users.js"
+import roomsRoute from "./routes/rooms.js"
 const app=express()
+dotenv.config()
+
+const connect=async()=>{
+try{
+  await mongoose.connect(process.env.MONGO)
+}catch(error){
+  throw error
+}
+}
+
+app.use("/api/auth",authRoute)
+app.use("/apihotels",hotelsRoute)
+app.use("/api/users",usersRoute)
+app.use("/api/rooms",roomsRoute)
 
 app.listen(8800,()=>{
+  connect()
   console.log("Connected to backend..")
 })
